982c477947d82e8f105eb4d8da2eedb011ca40c8
0261 <bada013@naver.com>
2020-05-04 01:13:26 +0900

    fix(home folder view): import dep

--- lib/components/HomeFolderView.dart
+++ lib/components/HomeFolderView.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeFolderGridView extends StatelessWidget {
   final Color boxColor;
