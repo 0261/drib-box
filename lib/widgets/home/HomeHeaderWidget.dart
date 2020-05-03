591aa6e03ca8a7d1cc35389bd8b161d2f51655e1
0261 <bada013@naver.com>
2020-05-04 01:10:21 +0900

    feat(home): header

--- lib/widgets/home/HomeHeaderWidget.dart
+++ lib/widgets/home/HomeHeaderWidget.dart
@@ -1,65 +1,37 @@
-import 'package:drib_box/components/HomeHeaderCilpper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeHeaderWidget extends StatelessWidget {
   const HomeHeaderWidget({
     Key key,
-    @required this.size,
   }) : super(key: key);
-
-  final Size size;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        ClipPath(
-          clipper: MyHeaderClipper(),
-          child: Container(
-            height: size.height * 0.45,
-            decoration: BoxDecoration(
-              color: Color(0xFFEEF7FE),
-            ),
+    return Container(
+      padding: const EdgeInsets.only(bottom: 48),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(CupertinoIcons.back),
           ),
-        ),
-        Positioned(
-          child: SvgPicture.asset('assets/icons/cloud.svg'),
-          top: size.height * 0.05,
-          left: size.width * 0.08,
-          width: 37,
-          height: 21,
-        ),
-        Positioned(
-          child: SvgPicture.asset('assets/icons/cloud.svg'),
-          top: size.height * 0.1,
-          left: size.width * 0.58,
-          width: 37,
-          height: 21,
-        ),
-        Positioned(
-          child: SvgPicture.asset('assets/icons/cloud.svg'),
-          top: size.height * 0.2,
-          left: size.width * -0.02,
-          width: 37,
-          height: 21,
-        ),
-        Positioned(
-          child: SvgPicture.asset('assets/icons/cloud.svg'),
-          top: size.height * 0.15,
-          left: size.width * 0.78,
-          width: 115,
-          height: 65,
-        ),
-        Positioned(
-            child: SvgPicture.asset(
-              'assets/icons/dripbox.svg',
-              width: size.width * 0.25,
-              height: size.height * 0.25,
-            ),
-            top: size.height * 0.15,
-            left: size.width * 0.05)
-      ],
+          Text(
+            '내 프로필',
+            style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w800,
+                color: Color(0xFF22215B)),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Icon(CupertinoIcons.ellipsis),
+          ),
+        ],
+      ),
     );
   }
 }
